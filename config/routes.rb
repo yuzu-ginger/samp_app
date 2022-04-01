be56506b4e2b34f8e7e37385ccf67a5f888a4d98
EC2 Default User <ec2-user@ip-172-31-89-77.ec2.internal>
2022-04-01 07:15:44 +0000

    [Add] chapter5

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 end
